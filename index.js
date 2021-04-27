6e793b9d700c82c73090edd504f932d3a8ea3b3c
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-27 18:34:42 -0400

    commit tree 6442dfa3769528f0473dcaac853bd070334b7ba3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(job = "go to the office") {
+  return `This Monday, I will ${job}.`
+}
+
+function wrapAdjective(wrap = "*") {
+  return function(adj = "special") {
+    return `You are ${wrap}${adj}${wrap}!`
+  }
+}
+
+let Calculator = {}
+
+Calculator.add = function(){return 1+3}
+Calculator.subtract = function(){return 1-3}
+Calculator.multiply = function(){return 1*3}
+Calculator.divide = function(){return 10/5}
+
+function actionApplyer(int, array) {
+  if (array.length === 0) {
+    return int
+  } else {
+    return array[2](array[1](array[0](int)))
+  }
+}
